7555cfd90730f24437b0a9872b52b5adc8a7d9d5
Matthew <matbulat@gmail.com>
2018-06-27 12:52:28 +0100

    Included Week 19 examples

---
+++ src/Week_19/TestObjectOutputStream.java
@@ -0,0 +1,16 @@
+package Week_19;
+
+import java.io.*;
+
+public class TestObjectOutputStream {
+
+    public static void main(String[] args) throws IOException {
+        try (
+                ObjectOutputStream output
+                = new ObjectOutputStream(
+                        new FileOutputStream("resources/object.dat"))) {
+            Sandwich sandwich = new Sandwich("White","Edam");
+            output.writeObject(sandwich);
+        }
+    }
+}
